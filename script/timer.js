f6083815c1791be105149649c56748522e0efd51
Alexandre <alexandre.gerard.dev@gmail.com>
2022-12-30 17:00:34 +0100

    app end v1.0

--- script/timer.js
+++ script/timer.js
@@ -2,7 +2,8 @@
 
 const start_button = document.getElementById("start"),
   reset_button = document.getElementById("reset"),
-  input_time = document.getElementById("input-time");
+  input_time = document.getElementById("input-time"),
+  input_time_wrapper = document.querySelector(".input-time-wrapper");
 
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
@@ -18,22 +19,25 @@ let sec_dot = document.querySelector(".sec_dot");
 
 let interval = null;
 let remainingSeconds = 0;
+let ringtone = new Audio("../icons&ringtones/alarm.mp3");
 
 //listeners
 
 start_button.addEventListener("click", () => {
-  if (interval === null) {
-    remainingSeconds = input_time.value;
+  if (input_time.value > 1440) {
+    alert("Ask for an hours under 24h");
+    location.reload();
+  } else if (interval === null && input_time.value < 1441) {
+    remainingSeconds = input_time.value * 60;
     start();
+    input_time_wrapper.classList.add("active");
   } else {
-    input_time.value = remainingSeconds;
+    input_time.value = remainingSeconds / 60;
     stop();
   }
 });
-reset_button.addEventListener("click", () => {});
-
-input_time.addEventListener("", () => {
-  console.log(input_time.value);
+reset_button.addEventListener("click", () => {
+  location.reload();
 });
 
 //Function
@@ -64,10 +68,6 @@ function timer() {
 
 timer();
 
-function changeIcon(x) {
-  x.classList.toggle("fa-stop");
-}
-
 function start() {
   if (remainingSeconds === 0) return;
 
@@ -77,22 +77,28 @@ function start() {
 
     if (remainingSeconds === 0) {
       stop();
+      ringtone.play();
+      ringtone.loop = true;
     }
   }, 1000);
-  changeIcon();
 }
 
 function stop() {
   clearInterval(interval);
   interval = null;
-  changeIcon();
 }
 
 function restrictAlphabets(e) {
   var x = e.which || e.keycode;
   if (x >= 48 && x <= 57) {
     return true;
+  } else if ((x = 190)) {
+    return true;
   } else {
     return false;
   }
+}
+
+function changeIcon(x) {
+  x.classList.toggle("fa-stop");
 }
